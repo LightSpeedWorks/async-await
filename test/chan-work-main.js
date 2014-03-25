181c1aa03561c75c8f4cdd57e0539ae14d846fb2
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-03-25 20:47:43 +0900

    rename file name

--- test/chan-work-main.js
+++ test/chan-work-main.js
@@ -4,8 +4,6 @@
 
 var makeChan = require('../lib/chan');
 
-var ch = makeChan();
-
 function cb(err, val) {
   if (err) {
     console.log('recv: '+ err);
@@ -18,17 +16,165 @@ function cb(err, val) {
   }
 } // cb
 
-ch(12);
-ch(34);
+function cb2(err, val) {
+  if (err) {
+    console.log('sent: '+ err);
+  }
+  else if (typeof val === 'object' && val !== null && ch.empty === val) {
+    console.log('sent: empty');
+  }
+  else {
+    console.log('sent: val = ' + val);
+  }
+} // cb2
 
-ch(cb);
-ch(cb);
-ch(cb);
+var ch = makeChan();
+console.log('ch 1');
+ch(1); // send
+console.log('ch 2');
+ch(2); // send
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch 3');
+ch(3); // send
+console.log('ch.end');
+ch.end(); // end
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+try {
+  console.log('ch 4');
+  ch(4); // send
+} catch(err) {
+  console.log(err + ': 4 cant send')
+}
+console.log();
 
-ch(56);
+var ch = makeChan();
+console.log('ch 1');
+ch(1)(cb2); // send
+console.log('ch 2');
+ch(2)(cb2); // send
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch 3');
+ch(3)(cb2); // send
+console.log('ch.end');
+ch.end(); // end
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+try {
+  console.log('ch 4');
+  ch(4)(cb2); // send
+} catch(err) {
+  console.log(err + ': 4 cant send')
+}
+console.log();
 
-ch.end();
-ch(cb);
-ch(cb);
+var ch = makeChan(null, 1);
+console.log('ch 1');
+ch(1)(cb2); // send
+console.log('ch 2');
+ch(2)(cb2); // send
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch 3');
+ch(3)(cb2); // send
+console.log('ch.end');
+ch.end(); // end
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log();
 
-// ch(78);
+var ch = makeChan(null, 2);
+console.log('ch 1');
+ch(1)(cb2); // send
+console.log('ch 2');
+ch(2)(cb2); // send
+console.log('ch 3');
+ch(3)(cb2); // send
+console.log('ch 4');
+ch(4)(cb2); // send
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch 5');
+ch(5)(cb2); // send
+console.log('ch 6');
+ch(6)(cb2); // send
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch.end');
+ch.end(); // end
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log();
+
+
+var ch = makeChan(null, Infinity);
+console.log('ch 1');
+ch(1)(cb2); // send
+console.log('ch 2');
+ch(2)(cb2); // send
+console.log('ch 3');
+ch(3)(cb2); // send
+console.log('ch 4');
+ch(4)(cb2); // send
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch 5');
+ch(5)(cb2); // send
+console.log('ch 6');
+ch(6)(cb2); // send
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log('ch.end');
+ch.end(); // end
+console.log('ch');
+ch(cb); // recv
+console.log('ch');
+ch(cb); // recv
+console.log();
+
