9110734900bb4eed1184d43684129fcef214d475
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-06-30 01:25:44 +0900

    merge chan into aa

--- aa.js
+++ aa.js
@@ -2,7 +2,6 @@
 
 this.aa = function () {
   var PromiseThunk = require('promise-thunk');
-  var chan = require('./chan');
   var isPromise = PromiseThunk.isPromise;
   var wrap = PromiseThunk.wrap;
 
@@ -40,6 +39,11 @@ this.aa = function () {
         writable: true, configurable: true}); } :
     function setValue(obj, prop, val) { obj[prop] = val; };
 
+  // setProto(obj, proto)
+  var setProto = Object.setPrototypeOf || {}.__proto__ ?
+    function setProto(obj, proto) { obj.__proto__ = proto; } : null;
+
+
   // nextTickDo(fn)
   var nextTickDo = typeof setImmediate === 'function' ? setImmediate :
     typeof process === 'object' && process && typeof process.nextTick === 'function' ? process.nextTick :
@@ -67,6 +71,7 @@ this.aa = function () {
     });
   }
 
+
   // aa - async-await
   function aa(gtor) {
     var ctx = this, args = slice.call(arguments, 1);
@@ -85,7 +90,7 @@ this.aa = function () {
 
     // is not generator?
     if (!isGenerator(gtor))
-      return chan.apply(ctx, arguments);
+      return Channel.apply(ctx, arguments);
 
     var resolve, reject, p = PromiseThunk(
       function (res, rej) { resolve = res; reject = rej; });
@@ -184,6 +189,7 @@ this.aa = function () {
     }
   }
 
+
   // isGeneratorFunction
   function isGeneratorFunction(gtor) {
     return gtor instanceof GeneratorFunction;
@@ -194,6 +200,159 @@ this.aa = function () {
     return gtor instanceof GeneratorFunctionPrototype || !!gtor && gtor.next === 'function';
   }
 
+
+  // Channel(empty)
+  // recv: chan(cb)
+  // send: chan(err, data)
+  // send: chan() or chan(undefined)
+  // send: chan(data)
+  // chan.end()
+  // chan.empty
+  // chan.done()
+  // chan.send(val or err)
+  // chan.recv(cb)
+
+  if (setProto)
+    setProto(Channel.prototype, Function.prototype);
+  else {
+    Channel.prototype = Function();
+    Channel.prototype.constructor = Channel;
+  }
+
+
+  // Channel(empty)
+  function Channel(empty) {
+    if (arguments.length > 1)
+      throw new Error('Channel: too many arguments');
+
+    channel.$isClosed = false;    // send stream is closed
+    channel.$isDone = false;      // receive stream is done
+    channel.$recvCallbacks = [];  // receive pending callbacks queue
+    channel.$sendValues    = [];  // send pending values
+
+    channel.empty = typeof empty === 'function' ? new empty() : empty;
+
+    if (setProto)
+      setProto(channel, Channel.prototype);
+    else {
+      channel.close = $$close;
+      channel.done  = $$done;
+      channel.send  = $$send;
+      channel.recv  = $$recv;
+
+      // for stream
+      channel.end      = $$close;
+      channel.stream   = $$stream;
+
+      if (channel.call !== Function.prototype.call)
+        channel.call = Function.prototype.call;
+
+      if (channel.apply !== Function.prototype.apply)
+        channel.apply = Function.prototype.apply;
+    }
+
+    return channel;
+
+    function channel(a, b) {
+      // yield callback
+      if (typeof a === 'function')
+        return channel.recv(a);
+
+      // error
+      if (a instanceof Error)
+        return channel.send(a);
+
+      // value or undefined
+      if (arguments.length <= 1)
+        return channel.send(a);
+
+      var args = slice.call(arguments);
+
+      if (a == null) {
+        if (arguments.length === 2)
+          return channel.send(b);
+        else
+          args.shift();
+      }
+
+      // (null, value,...) -> [value, ...]
+      return channel.send(args);
+    }
+
+  } // Channel
+
+  // send(val or err)
+  Channel.prototype.send = $$send;
+  function $$send(val) {
+    if (this.$isClosed)
+      throw new Error('Cannot send to closed channel');
+    else if (this.$recvCallbacks.length > 0)
+      complete(this.$recvCallbacks.shift(), val);
+    else
+      this.$sendValues.push(val);
+  } // send
+
+  // recv(cb)
+  Channel.prototype.recv = $$recv;
+  function $$recv(cb) {
+    if (this.done())
+      cb(null, this.empty);
+    else if (this.$sendValues.length > 0)
+      complete(cb, this.$sendValues.shift());
+    else
+      this.$recvCallbacks.push(cb);
+    return;
+  } // recv
+
+  // done()
+  Channel.prototype.done = $$done;
+  function $$done() {
+    if (!this.$isDone && this.$isClosed && this.$sendValues.length === 0) {
+      this.$isDone = true;
+      // complete each pending callback with the empty value
+      var empty = this.empty;
+      this.$recvCallbacks.forEach(function(cb) { complete(cb, empty); });
+    }
+
+    return this.$isDone;
+  } // done
+
+  // close() end()
+  Channel.prototype.close = $$close;
+  Channel.prototype.end = $$close;
+  function $$close() {
+    this.$isClosed = true;
+    return this.done();
+  } // close
+
+  // stream(stream)
+  Channel.prototype.stream = $$stream;
+  function $$stream(stream) {
+    var channel = this;
+    stream.on('end',      close);
+    stream.on('error',    error);
+    stream.on('readable', readable);
+    return this;
+
+    function close()    { return channel.close(); }
+    function error(err) { return channel.send(err); }
+
+    function readable() {
+      var buf = this.read();
+      if (!buf) return;
+      channel.send(buf);
+    } // readable
+  } // stream
+
+  // complete(cb, val or err)
+  function complete(cb, val) {
+    if (val instanceof Error)
+      cb(val);
+    else
+      cb(null, val);
+  } // complete
+
+
   if (typeof module === 'object' && module && module.exports)
     module.exports = aa;
 
@@ -201,9 +360,10 @@ this.aa = function () {
     aa.GeneratorFunction = GeneratorFunction;
 
   aa.isGeneratorFunction = isGeneratorFunction;
-  aa.isGenerator  = isGenerator;
-  aa.aa           = aa;
-  aa.chan         = chan;
+  aa.isGenerator         = isGenerator;
+  aa.aa                  = aa;
+  aa.chan                = Channel;
+  aa.Channel             = Channel;
 
   if (Object.getOwnPropertyNames)
     Object.getOwnPropertyNames(PromiseThunk).forEach(function (prop) {
